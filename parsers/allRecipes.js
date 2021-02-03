952dfe72d74b30694a1603b9bd0f57831beb30b4
dedolence <44186443+dedolence@users.noreply.github.com>
2021-02-02 23:32:00 -0500

    inrgedients done

--- parsers/allRecipes.js
+++ parsers/allRecipes.js
@@ -80,30 +80,74 @@ function printInfo(element) {
 exports.parse = function(recipe) {
     if (!recipe.dom) throw "Error: DOM property undefined.";
     else {
-        recipe.title = _getTitle(recipe.dom);
-        console.log(recipe.title);
-        recipe.ingredients = _getIngredients(recipe.dom);
+        let _meta = _getMeta(recipe.dom);
+        recipe.ingredients = _getIngredients(_meta[1].recipeIngredient);
+        console.log(recipe.ingredients);
     }
 }
 
-function _getTitle(node) {
-    let _e = _findHtmlElement(['name', 'title'], node, true, 1)[0]
-        .children[0]
-        .data;
 
-    // filter helper
-    let _rList = ["recipe", "|", "allrecipes"];
-    let _rFilter = function(_w) {
-        return !_rList.includes(_w.toLowerCase());
-    }
+function _getMeta(_node) {
+    let _head = _findHtmlElement(['name', 'head'], _node, true, 1)[0];
+    let _m = _findAttribute('type', 'application/ld+json', _head);
+    let _mData = _m[0].children[0].data;
 
-    // split string, filter out irrelevant title decoration, reformat to string
-    return _e.split(/\s+/)
-        .filter(_rFilter)
-        .join(' ');
+    return JSON.parse(_mData);
 }
 
-function _getIngredients(node) {
+
+function _findAttribute(key, value, node) {
+    let _k = key;
+    let _v = value;
+    let _node = node;
+    let _results = [];
+
+    if (_node.attribs) {
+        for (_a in _node.attribs) {
+            if (_a === _k && _node.attribs[_a] === _v) {
+                _results.push(_node);
+            }
+        }
+    }
+
+    if (_node.children) {
+        for (let _i = 0; _i < _node.children.length; _i++) {
+            let _c = _findAttribute(_k, _v, _node.children[_i]);
+            _results.push.apply(_results, _c);
+        }
+    }
+
+    return _results;
+}
+
+function _getIngredients(ingredientsArray) {
+    return ingredientsArray.map(_i => {
+        // extract quantity; i'm sure there's a pithy one-liner that could do this, oh well
+        let _a = _i.split(/\s+/);
+        let _b = {}
+        if (!isNaN(_a[0]) || _Fractions[_a[0]]) {
+            _b.quantity = _a.shift();
+        }
+        // TODO: clean up this conversion of fraction symbol to plain numeric
+        if (_Fractions[_b.quantity]) {
+            _b.quantity = _Fractions[_b.quantity];
+        }
+        _b.name = _a.join(' ');
+        return _b;
+    })
+    .map(_i => {
+        // extract unit of measurement. the slice is to account for plural 's', e.g. teaspoons
+        let _a = _i.name.split(/\s+/);
+        let _u = _a.shift().toString().toLowerCase();
+        if (_Units.includes(_u) || _Units.includes(_u.slice(0, -1))) {
+            _i.unit = _u;
+            _i.name = _a.join(' ');
+        }
+        return _i;
+    });
+}
+
+function _getIngredients1(node) {
     // array of strings representing ingredients
     return _findClass(_classKeys.ingredients, node)
         .map(_i => _i.children[0].data.trim())  // remove newlines and whitespace
@@ -131,6 +175,55 @@ function _getIngredients(node) {
             }
             return _i;
         });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _getTitle(node) {
+    let _e = _findHtmlElement(['name', 'title'], node, true, 1)[0]
+        .children[0]
+        .data;
+
+    // filter helper
+    let _rList = ["recipe", "|", "allrecipes"];
+    let _rFilter = function(_w) {
+        return !_rList.includes(_w.toLowerCase());
+    }
+
+    // split string, filter out irrelevant title decoration, reformat to string
+    return _e.split(/\s+/)
+        .filter(_rFilter)
+        .join(' ');
 }
 
 // TODO: add this to a generic library, make pull request
@@ -183,18 +276,13 @@ function _findHtmlElement(searchProp, node, recurse, limit) {
 
     // iterate through each property of _node object
     for (let _prop in _node) {
-        // see if the key and value matches this property
+
         if (_prop == _k && _node[_prop] == _v) {
-
-            // store the entire node in results
             _result.push(_node);
-
-            // make sure limit hasn't been reached
             if (--limit <= 0)
                 break;
         }
 
-        // check children
         if (recurse && _prop == 'children') {
             for (let _i = 0; _i < _node.children.length; _i++) {
                 let children = _findHtmlElement(searchProp, _node.children[_i], recurse, limit);
@@ -205,5 +293,6 @@ function _findHtmlElement(searchProp, node, recurse, limit) {
             }
         }
     }
+
     return _result;
 }
